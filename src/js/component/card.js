a32a8cf690808c0efe580f2aa0f263148f55aa55
sarasempere <semperesara@gmail.com>
2021-06-11 17:24:25 +0000

    arreglado error que ayer no existía

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -2,21 +2,21 @@ import React from "react";
 
 export function Card() {
 	return (
-		<div class="feature col">
-			<div class="feature-icon bg-primary bg-gradient">
-				<svg class="bi" width="1em" height="1em">
+		<div className="feature col">
+			<div className="feature-icon bg-primary bg-gradient">
+				<svg className="bi" width="1em" height="1em">
 					<use xlinkHref="#people-circle"></use>
 				</svg>
 			</div>
 			<h2>Featured title</h2>
 			<p>
 				Paragraph of text beneath the heading to explain the heading.
-				We'll add onto it with another sentence and probably just keep
+				Well add onto it with another sentence and probably just keep
 				going until we run out of words.
 			</p>
-			<a href="#" class="icon-link">
+			<a href="#" className="icon-link">
 				Call to action
-				<svg class="bi" width="1em" height="1em">
+				<svg className="bi" width="1em" height="1em">
 					<use xlinkHref="#chevron-right"></use>
 				</svg>
 			</a>
